4b4e06ebd9921fb4c2e8aad4c84c08ce7d1f89aa
can <can.dvg@softworld.jp>
2025-01-15 11:27:00 +0700

    assignment_30

--- leet_code/assignment_21_30.py
+++ leet_code/assignment_21_30.py
@@ -20,5 +20,60 @@ def generate_parenthesis(n):
     back_track("", 0, 0)
     return result 
 
-test = generate_parenthesis(3)
-print(0)
+
+"""Assignment 30: Substring with Concatenation of All Words 
+
+You are given a string s and an array of strings words. 
+All the strings of words are of the same length.
+A concatenated string is a string that exactly contains all the strings of any permutation of words concatenated.
+
+Example 1: 
+- Input:  s = "barfoothefoobarman", words = ["foo","bar"]
+- Ouput: [0, 9]
+- Explaination: 
+    + The substring starting at 0 is "barfoo". It is the concatenation of ["bar","foo"] which is a permutation of words.
+    + The substring starting at 9 is "foobar". It is the concatenation of ["foo","bar"] which is a permutation of words.
+
+"""
+from itertools import permutations
+from collections import Counter
+def find_substring(s, words):
+    permutate_lst = ["".join(word) for word in permutations(words)]
+    len_words = len(permutate_lst[0])
+    result = []
+    idx = 0 
+    while True: 
+        if idx >= len(s):
+            break 
+        if s[idx:idx+len_words] in permutate_lst: 
+            result.append(idx)
+        idx += 1
+    return result 
+
+def find_substring(s, words): 
+    word_len = len(words[0])
+    word_count = len(words)
+    total_len = word_len * word_count
+    word_freq = Counter(words) # count the frequency of each word in list 
+
+    result = []
+    for i in range(word_len):
+        left = i 
+        right = i 
+        current_freq = Counter()
+        while right + word_len <= len(s):
+            word = s[right : right + word_len]
+            right += word_len   # update right index 
+            if word in word_freq:
+                current_freq[word] += 1 
+
+                while current_freq[word] > word_freq[word]:
+                    current_freq[s[left:left + word_len]] -= 1
+                    left += word_len
+                
+                if right - left == total_len:
+                    result.append(left)
+            else: 
+                current_freq.clear()
+                left = right 
+    return result
