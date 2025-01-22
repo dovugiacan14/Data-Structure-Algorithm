738849bf875ee40a59e4a71f645a044c0893c2b0
can <can.dvg@softworld.jp>
2025-01-22 10:59:14 +0700

    add assignment 54

--- LeetCode/assignment_51_60.py
+++ LeetCode/assignment_51_60.py
@@ -162,4 +162,47 @@ def max_subarray(nums):
             current_sum = 0 
     return max_sum 
 
-print(max_subarray([-2,1,-3,4,-1,2,1,-5,4]))
+
+"""Assignment 54: Spiral Matrix 
+
+Given an m x n matrix, return all elements of the matrix in spiral order.
+
+Example 1: 
+- Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+- Ouput: [1,2,3,6,9,8,7,4,5]
+
+Example 2: 
+- Input: [[1,2,3,4],[5,6,7,8],[9,10,11,12]] 
+- Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+"""
+def spiral_order(matrix):
+    if not matrix:
+        return []
+        
+    res = []
+    start_row, end_row = 0, len(matrix) - 1
+    start_col, end_col = 0, len(matrix[0]) - 1
+    
+    while start_row <= end_row and start_col <= end_col:
+        for i in range(start_col, end_col + 1):
+            res.append(matrix[start_row][i])
+        start_row += 1
+        
+        for i in range(start_row, end_row + 1):
+            res.append(matrix[i][end_col])
+        end_col -= 1
+        
+        if start_row <= end_row:
+            for i in range(end_col, start_col - 1, -1):
+                res.append(matrix[end_row][i])
+            end_row -= 1
+        
+        if start_col <= end_col:
+            for i in range(end_row, start_row - 1, -1):
+                res.append(matrix[i][start_col])
+            start_col += 1
+            
+    return res
+
+result = spiral_order([[1,2,3],[4,5,6],[7,8,9],[10,11,12]])
+print(result)
