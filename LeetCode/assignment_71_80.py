52d8e973efcc6ce65c6852ce6aaa5bb5d4f0ac5d
Windows-key <giacanemail@gmail.com>
2025-02-08 10:03:51 +0700

    add assignment 79

--- LeetCode/assignment_71_80.py
+++ LeetCode/assignment_71_80.py
@@ -216,3 +216,57 @@ def subsets(nums):
     return result 
 
 print(subsets([1,2,3]))
+
+
+# có thời gian hãy xem và debug lại nhé.
+"""Assignment 79: Word Search
+
+Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring.
+The same letter cell may not be used more than once.
+
+Example 1: 
+- Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+- Output: true
+
+Example 2: 
+- nput: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+- Output: false
+ 
+"""
+def word_search(board, word):
+    if not board or not board[0]:
+        return False 
+    
+    rows = len(board)
+    cols = len(board[0])
+    word_len = len(word)
+
+    def backtrack(row, col, index): 
+        if index == word_len:
+            return True 
+        
+        if row < 0 or row >= rows or col < 0 or col >= cols or board[row][col] != word[index]:
+            return False  
+
+        temp, board[row][col] = board[row][col], "#"
+
+        found = (
+            backtrack(row + 1, col, index+1) or
+            backtrack(row - 1, col, index + 1) or
+            backtrack(row, col + 1, index + 1) or
+            backtrack(row, col - 1 , index + 1) 
+        ) 
+        board[row][col] = temp 
+        return found 
+
+    for i in range(rows): 
+        for j in range(cols):
+            if board[i][j] == word[0] and backtrack(i, j, 0): 
+                return True 
+    return False
+
+board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
+word = "ABCCED"
+
+print(word_search(board, word))
