48d2ad25d9f042bd5b5e12fcefe59cf5f10c9ca1
can <can.dvg@softworld.jp>
2025-02-11 09:26:48 +0700

    add assignment 82

--- LeetCode/assignment_81_90.py
+++ LeetCode/assignment_81_90.py
@@ -41,3 +41,44 @@ def delete_duplicates(head):
         cur.next = ListNode(val)
         cur = cur.next
     return new_head
+
+"""Assignment 82: Remove Duplicates from Sorted List 
+
+Given the head of a sorted linked list, delete all duplicates such that each element appears only once.
+Return the linked list sorted as well.
+
+Example 1: 
+- Input: head = [1,1,2]
+- Output: [1,2]
+
+Example 2: 
+- Input: head = [1,1,2,3,3]
+- Output: [1,2,3]
+"""
+
+class ListNode:
+    def __init__(self, val= 0, next= None):
+        self.val = val 
+        self.next= next
+
+def delete_duplicates(head): 
+    # convert list node to array 
+    tmp_arr = list()
+    result_arr = list()
+    if not head: 
+        return None 
+    while head: 
+        tmp_arr.append(head.val)
+        head = head.next 
+    
+    result_arr = list(set(tmp_arr))
+    if not result_arr: 
+        return None 
+    result_arr.sort()
+    # convert result to node 
+    new_head = ListNode(result_arr[0])
+    cur = new_head
+    for val in result_arr[1:]:
+        cur.next = ListNode(val)
+        cur = cur.next
+    return new_head
