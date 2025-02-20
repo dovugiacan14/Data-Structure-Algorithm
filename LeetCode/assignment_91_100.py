6da99ac7e89a18b1ed34c448daebfe6db0170948
can <can.dvg@softworld.jp>
2025-02-20 10:41:04 +0700

    add assignment 93

--- LeetCode/assignment_91_100.py
+++ LeetCode/assignment_91_100.py
@@ -128,3 +128,45 @@ def reverseBetween(head: ListNode, left: int, right: int) -> ListNode:
         temp.next = prev.next
         prev.next = temp  # Cập nhật node đầu của đoạn bị đảo
     return dummy.next
+
+"""Assignment 93: Restore IP Addresses
+
+A valid IP address consists of exactly four integers separated by single dots. Each integer is between 0 and 255 (inclusive) and cannot have leading zeros.
+
+For example, "0.1.2.201" and "192.168.1.1" are valid IP addresses, but "0.011.255.245", "192.168.1.312" and "192.168@1.1" are invalid IP addresses.
+Given a string s containing only digits, return all possible valid IP addresses that can be formed by inserting dots into s. 
+You are not allowed to reorder or remove any digits in s. You may return the valid IP addresses in any order.
+
+- Input:s = "25525511135"
+- Output: ["255.255.11.135","255.255.111.35"]
+
+Idea: 
+- Split s into 4 part 
+- Check each part is valid ? If invalid then backtrack to previous step
+"""
+def restore_ip_address(s): 
+    result = []
+    if len(s) < 3 or len(s) > 12: 
+        return result
+
+    def backtrack(start, path): 
+        # if enough 4 part or reach the length s 
+        if len(path) == 4 and start == len(s): 
+            result.append(".".join(path))
+            return 
+        
+        if len(path) == 4 or start == len(s): 
+            return 
+        
+        for length in range(1,4): 
+            if start + length > len(s):  # Tránh lỗi IndexError
+                break  
+            part = s[start:start + length]
+
+            # check condition 
+            if (part.startswith('0') and len(part) > 1) or int(part) > 255: 
+                continue 
+            backtrack(start + length, path + [part])
+
+    backtrack(0, [])
+    return result
