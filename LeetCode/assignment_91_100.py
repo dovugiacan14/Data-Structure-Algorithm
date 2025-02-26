f3ea895aee1384cb44064d78c0b75269736a8326
Windows-key <giacanemail@gmail.com>
2025-02-26 13:55:12 +0700

    add assignment 99

--- LeetCode/assignment_91_100.py
+++ LeetCode/assignment_91_100.py
@@ -430,7 +430,6 @@ Example 2:
     - Output: False
 """
 
-
 class TreeNode(object):
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -453,7 +452,57 @@ def is_binary_search_tree(root):
             node.right, node.val, max_val
         )
 
-    # min_val = float('-inf')
-    # max_val = float('inf')
-    # result = check(root, min_val, max_val)
     return check(root, float("-inf"), float("inf"))
+
+
+"""Assignment 99: Recover Binary Search Tree 
+
+You are given the root of a binary search tree (BST), where the values of exactly two nodes of the tree were swapped by mistake. 
+Recover the tree without changing its structure.
+
+Example 1: 
+    - Input: root = [1,3,null,null,2]
+    - Output: [3,1,null,null,2]
+    - Explaination: 3 cannot be a left child of 1 because 3 > 1. Swapping 1 and 3 makes the BST valid.
+
+Example 2: 
+    - Input: root = [3,1,4,null,null,2]
+    - Output: [2,1,4,null,null,3]
+    - Explaination: 2 cannot be in the right subtree of 3 because 2 < 3. Swapping 2 and 3 makes the BST valid.
+
+"""
+class TreeNode(object): 
+    def __init__(self, val= 0, left= None, right= None): 
+        self.val = val
+        self.left = left 
+        self.right = right 
+
+def recover_tree(self, root): 
+    """
+    :type root: Optional[TreeNode]
+    :rtype: None Do not return anything, modify root in-place instead.
+    """
+    first = None 
+    second = None 
+    prev = None 
+
+    def inoder_traversal(node):
+        nonlocal first, second, prev
+        if not node: 
+            return 
+        
+        inoder_traversal(node.left) # visit left subtree
+
+        if prev and prev.val > node.val: 
+            if not first: 
+                first = prev # mark the first error 
+            second = node  # mark the second error 
+        prev = node 
+
+        inorder_traversal(node.right)
+
+    inoder_traversal(root)
+    
+    # swap two errors node 
+    if first and second: 
+        first.val, second.val = second.val, first.val  
