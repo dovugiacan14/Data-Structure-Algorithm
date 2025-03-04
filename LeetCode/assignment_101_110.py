7eaf7825d5e10de087397c5bcb9fb8b75f4b650f
can <can.dvg@softworld.jp>
2025-03-04 11:04:42 +0700

    add assugnment 106

--- LeetCode/assignment_101_110.py
+++ LeetCode/assignment_101_110.py
@@ -1,3 +1,11 @@
+"""Note
+
+- Inorder: Left -> Node -> Right (LNR)
+- Preorder: Left -> Right -> Node (LRN) 
+- Postorder: Node -> Left -> Right (NLR)
+
+"""
+
 """Assignment 101: Symmetric Tree
 
 Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
@@ -179,6 +187,47 @@ Example 2:
 
 """
 from typing import List, Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        inorder_map = {val: idx for idx, val in enumerate(inorder)} 
+        pre_idx = 0  
+
+        def helper(left: int, right: int) -> Optional[TreeNode]:
+            nonlocal pre_idx
+            if left > right:
+                return None  
+
+            root_val = preorder[pre_idx]  
+            pre_idx += 1 
+            root = TreeNode(root_val)
+
+            root.left = helper(left, inorder_map[root_val] - 1)
+            root.right = helper(inorder_map[root_val] + 1, right)
+
+            return root
+
+        return helper(0, len(inorder) - 1)
+    
+"""Assignment 106: Construct Binary Tree from Inorder and Postorder Traversal
+
+Given two integer arrays inorder and postorder where inorder is the inorder traversal of a binary tree and 
+postorder is the postorder traversal of the same tree, construct and return the binary tree.
+
+Example 1: 
+    - Input: inorder = [9,3,15,20,7], postorder = [9,15,7,20,3]
+    - Output:  [3,9,20,null,null,15,7]
+
+Example 2: 
+    - Input: inorder = [-1], postorder = [-1]
+    - Output: [-1]
+"""
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
