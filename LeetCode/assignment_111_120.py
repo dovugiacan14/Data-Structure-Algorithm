e9424c9bf2b4dd8b0dafa6b6f51a44206a977341
can <can.dvg@softworld.jp>
2025-03-10 09:06:33 +0700

    add assignement 111 and 112

--- LeetCode/assignment_111_120.py
+++ LeetCode/assignment_111_120.py
@@ -1,4 +1,5 @@
 from collections import deque
+
 """Assignment 111: 
 
 Given a binary tree, find its minimum depth.
@@ -13,25 +14,58 @@ Example 2:
     - Output:5 
 
 """
-class TreeNode(object): 
-    def __init__(self, val= 0, left= None, right= None): 
-        self.val = val 
-        self.left = left 
-        self.right = right 
-    
-def min_depth(root): 
-    result = 0 
-    if root: 
-        queue = deque([root]) 
-        while queue: 
-            level_size = len(queue) 
-            for _ in range(level_size): 
-                nodes = queue.popleft()
 
-                if nodes.left: 
-                    queue.append(nodes.left) 
-                if nodes.right: 
-                    queue.append(nodes.right) 
-            result += 1 
-            
-    return result 
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def min_depth(root):
+    if not root:
+        return 0
+
+    queue = deque([root, 1])  # (node, depth)
+
+    while queue:
+        node, depth = queue.popleft()
+
+        # if leaf node, return depth immediately
+        if not node.left and not node.right:
+            return depth
+
+        if node.left:
+            queue.append((node.left, depth + 1))
+        if node.right:
+            queue.append((node.right, depth + 1))
+
+    return 0
+
+
+"""Assignment 112: Path Sum 
+Given the root of a binary tree and an integer targetSum, 
+return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+A leaf is a node with no children.
+
+Example 1: 
+    - Input:  root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+    - Output: True
+
+Example 2: 
+    - Input: root = [1, 2, 3]], targetSum = 5
+    - Output: False 
+
+"""
+
+
+def has_path_sum(root, targetSum):
+    if not root:
+        return False
+
+    targetSum -= root.val
+    if not root.left and not root.right:
+        return targetSum == 0
+
+    return has_path_sum(root.left, targetSum) or has_path_sum(root.right, targetSum)
