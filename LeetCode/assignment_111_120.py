21154b72e5095173f323bbe11c9dcf1886b720af
can <can.dvg@softworld.jp>
2025-03-12 10:11:21 +0700

    add assigment 114

--- LeetCode/assignment_111_120.py
+++ LeetCode/assignment_111_120.py
@@ -103,3 +103,46 @@ def pathSum(root, targetSum):
     
     process(root, targetSum, [], 0)
     return result
+
+
+"""Assignment 114. Flatten Binary Tree to Linked List
+
+Given the root of a binary tree, flatten the tree into a "linked list":
+    - The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
+    - The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+
+Example 1: 
+    - Input: root = [1,2,5,3,4,null,6]
+    - Output: [1,null,2,null,3,null,4,null,5,null,6]
+
+"""
+def flatten(root): 
+    if not root: 
+        return []
+    
+    # traversal pre-orrder
+    stack = [root]
+    prev = None 
+
+    while stack: 
+        node = stack.pop()
+
+        if prev: 
+            prev.right = node 
+            prev.left = None  # set left to None 
+
+        if node.right: 
+            stack.append(node.right)
+        if node.left: 
+            stack.append(node.left)
+        
+        prev= node 
+    
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(5)
+root.left.left = TreeNode(3)
+root.left.right = TreeNode(4)
+root.right.right = TreeNode(6)
+flatten(root)
