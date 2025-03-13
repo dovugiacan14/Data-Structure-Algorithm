5f2ad9183b180ee6430cc3350dd169bc941c1177
can <can.dvg@softworld.jp>
2025-03-13 10:59:51 +0700

    add assignment 116

--- LeetCode/assignment_111_120.py
+++ LeetCode/assignment_111_120.py
@@ -146,3 +146,52 @@ root.left.left = TreeNode(3)
 root.left.right = TreeNode(4)
 root.right.right = TreeNode(6)
 flatten(root)
+
+"""Assignment 116. Populating Next Right Pointers in Each Node 
+
+You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+Initially, all next pointers are set to NULL.
+
+Example 1: 
+    - Input: root = [1,2,3,4,5,6,7]
+    - Output: [1,#,2,3,#,4,5,6,7,#]
+    - Explaination: Given the above perfect binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B. 
+    The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
+
+"""
+class Node(object):
+    def __init__(self, val=0, left=None, right=None, next=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+def connect(root):
+    head = root 
+    while head and head.left: 
+        curr = head  
+        while curr: 
+            curr.left.next = curr.right   # link left child -> right child 
+            if curr.next: 
+                curr.right.next = curr.next.left 
+            curr = curr.next   # move to next node in the current level 
+        head = head.left
+    return root 
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+root.right.left = Node(6)
+root.right.right = Node(7)
+
+connect(root)
