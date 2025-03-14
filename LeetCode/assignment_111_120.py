79fa6fb0a4f3b7249c65e09e088a936250f972e8
can <can.dvg@softworld.jp>
2025-03-14 09:54:52 +0700

    add assignment 117

--- LeetCode/assignment_111_120.py
+++ LeetCode/assignment_111_120.py
@@ -195,3 +195,46 @@ root.right.left = Node(6)
 root.right.right = Node(7)
 
 connect(root)
+
+"""Assignment 117. Populating Next Right Pointers in Each Node II.
+
+Given a binary tree
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+
+Example 1: 
+    - Input: root = [1,2,3,4,5,null,7]
+    - Output: [1,#,2,3,#,4,5,7,#] 
+    - Explaination: Given the above binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B.
+      The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
+"""
+def connect2(root):
+    if not root: 
+        return None 
+    
+    queue = deque([root])
+    while queue: 
+        prev = None 
+        level_size = len(queue)
+
+        for _ in range(level_size):
+            node = queue.popleft()
+
+            if prev: 
+                prev.next = node    # connect previous node to current node 
+            
+            prev = node 
+            if node.left: 
+                queue.append(node.left)
+            if node.right: 
+                queue.append(node.right)
+        node.next = None 
+    return root
