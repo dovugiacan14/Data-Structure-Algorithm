8637f5d7fd5a31d31580fb6d3011c818d32ab44d
can <can.dvg@softworld.jp>
2025-03-17 09:50:38 +0700

    add asignment 120

--- LeetCode/assignment_111_120.py
+++ LeetCode/assignment_111_120.py
@@ -272,7 +272,7 @@ Example 1:
     - Output: [1,3,3,1]
 """
 def get_row(rowIndex): 
-    num_row = rowIndex 
+    num_row = rowIndex + 1
     res = [[1]]
     for _ in range(1, num_row): 
         prev_row = res[-1]
@@ -283,3 +283,42 @@ def get_row(rowIndex):
         new_row.append(1)
         res.append(new_row)
     return res[-1]
+
+
+"""Assignment 120. Triangle
+Given a triangle array, return the minimum path sum from top to bottom.
+For each step, you may move to an adjacent number of the row below. 
+More formally, if you are on index i on the current row, you may move to either index i or index i + 1 on the next row.
+
+Example 1:
+    - Input: triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
+    - Output: 11
+"""
+def minimum_total(triangle): 
+    total = triangle[0][0]
+    cur_index = 0 
+    for i in range(1, len(triangle)): 
+        if triangle[i][cur_index] < triangle[i][cur_index + 1]:  
+            min_par = triangle[i][cur_index]
+        else:
+            min_par = triangle[i][cur_index + 1]
+            cur_index += 1 
+
+        total += min_par 
+
+    return total 
+
+# use dynamic programming 
+def minimum_total(triangle): 
+    n = len(triangle)
+    dp = triangle[-1][:]
+
+    for i in range(n - 2, -1, -1): 
+        for j in range(len(triangle[i])): 
+            # choose the minimum path 
+            dp[j] = triangle[i][j] + min(dp[j], dp[j + 1])
+
+    return dp[0]
+
+triangle =  [[2],[3,4],[6,5,7],[4,1,8,3]] 
+print(minimum_total(triangle))
