5df71df32ba882220fe55c05943f81abbe1cd76b
Windows-key <giacanemail@gmail.com>
2025-03-20 22:30:20 +0700

    add assignment 122

--- LeetCode/assignment_121_130.py
+++ LeetCode/assignment_121_130.py
@@ -21,7 +21,7 @@ def max_profit(prices):
         
     return res 
 
-"""Assignment 121. Best Time to Buy and Sell Stock II
+"""Assignment 122. Best Time to Buy and Sell Stock II
 
 You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
 On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. 
@@ -44,3 +44,29 @@ def max_profit_v2(prices):
     return profit
 
 print(max_profit_v2([1,2,3,4,5]))
+
+"""Assignment 123. Best Time to Buy and Sell Stock III.
+
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+Find the maximum profit you can achieve. You may complete at most two transactions.
+Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+
+Example 1:
+    - Input: prices = [3,3,5,0,0,3,1,4]
+    - Output: 6
+    - Explaination:  
+        + Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+        + Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 = 3.
+"""
+def max_profit_v3(prices):
+    trans1, trans2 = float('inf'), float('inf')
+    profit1, profit2 = 0, 0 
+
+    for i in prices: 
+        trans1 = min(trans1, i)
+        profit1 = max(profit1, i - trans1)
+
+        trans2 = min(trans2, i - profit1)
+        profit2 = max(profit2, i - trans2)
+
+    return profit2
