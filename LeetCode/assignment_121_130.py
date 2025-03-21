ca58e82a831e27f4f1bcc00dbf1660f7e7bf7d2c
can <can.dvg@softworld.jp>
2025-03-21 14:00:19 +0700

    Add assignment 124

--- LeetCode/assignment_121_130.py
+++ LeetCode/assignment_121_130.py
@@ -70,3 +70,53 @@ def max_profit_v3(prices):
         profit2 = max(profit2, i - trans2)
 
     return profit2
+
+
+"""Assignment 124. Binary Tree Maximum Path Sum. 
+
+A path in a binary tree is a sequence of nodes where each pair of adjacent nodes in the sequence has an edge connecting them. 
+A node can only appear in the sequence at most once. Note that the path does not need to pass through the root.
+
+The path sum of a path is the sum of the node's values in the path.
+Given the root of a binary tree, return the maximum path sum of any non-empty path.
+Example 1:
+    - Input: root = [1,2,3]
+    - Output: 6
+    - Explaination: The optimal path is 2 -> 1 -> 3 with a path sum of 2 + 1 + 3 = 6.
+
+Example 2:
+    - Input: root = [-10,9,20,null,null,15,7]
+    - Output: 42
+    - Explaination:  The optimal path is 15 -> 20 -> 7 with a path sum of 15 + 20 + 7 = 42."
+"""
+class TreeNode: 
+    def __init__(self, val= 0, left= None, right= None): 
+        self.val  = val 
+        self.left = left
+        self.right = right 
+
+def max_path_sum(root):
+    global max_sum
+    max_sum = float('-inf') 
+
+    def dfs(node): 
+        if not node: 
+            return 0
+
+        left_gain = max(dfs(node.left), 0)
+        right_gain = max(dfs(node.right), 0)
+
+        global max_sum 
+        max_sum = max(max_sum, node.val + left_gain + right_gain)
+        return node.val + max(left_gain, right_gain)
+    
+    
+    dfs(root)
+    return max_sum
+
+root = TreeNode(-10)
+root.left = TreeNode(9)
+root.right = TreeNode(20)
+root.right.left = TreeNode(15)
+root.right.right = TreeNode(7)
+print(max_path_sum(root))
