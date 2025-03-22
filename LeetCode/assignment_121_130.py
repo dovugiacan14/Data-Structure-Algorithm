f1cebf40f5f1de4140c2f4bbf6da116126187121
Windows-key <giacanemail@gmail.com>
2025-03-22 12:05:57 +0700

    add assignment 125

--- LeetCode/assignment_121_130.py
+++ LeetCode/assignment_121_130.py
@@ -120,3 +120,38 @@ root.right = TreeNode(20)
 root.right.left = TreeNode(15)
 root.right.right = TreeNode(7)
 print(max_path_sum(root))
+
+"""Assignment 125. Valid Palindorme 
+
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. 
+Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+Example 1:
+    - Input: s = "A man, a plan, a canal: Panama"
+    - Output: true
+    - Explaination: "amanaplanacanalpanama" is a palindrome.
+
+Example 2:
+    - Input: s = "race a car"
+    - Output: false
+    - Explanation: "raceacar" is not a palindrome.
+"""
+def is_palindorme(s):
+    if not s: 
+        return True 
+    s = s.replace(',',"").replace(" ", "").replace(":", "").replace('.', "").replace("@", "").replace("#", "").replace("_", "")
+    s = s.lower()
+    reversed_text = s[::-1]
+    if s == reversed_text:
+        return True 
+    return False
+
+import re
+def is_palindorme(s):
+    s = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
+    return s == s[::-1]
+    
+
+print(is_palindorme("race a car"))
+
