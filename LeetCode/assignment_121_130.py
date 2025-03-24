97e9197a1206da6ec0fec113efcb11f0fd97f545
can <can.dvg@softworld.jp>
2025-03-24 18:42:12 +0700

    add assignment 129

--- LeetCode/assignment_121_130.py
+++ LeetCode/assignment_121_130.py
@@ -154,3 +154,72 @@ def is_palindorme(s):
     
 
 print(is_palindorme("race a car"))
+
+"""Assignment 129. Sum Root to Leaf Numbers
+
+You are given the root of a binary tree containing digits from 0 to 9 only.
+Each root-to-leaf path in the tree represents a number.
+
+For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
+Return the total sum of all root-to-leaf numbers. Test cases are generated so that the answer will fit in a 32-bit integer.
+
+A leaf node is a node with no children.
+
+Example 1:
+    - Input: root = [1,2,3]
+    - Output: 25
+    - Explaination: 
+        + The root-to-leaf path 1->2 represents the number 12.
+        + The root-to-leaf path 1->3 represents the number 13.
+        + Therefore, sum = 12 + 13 = 25.
+
+Example 2:
+    - Input: root = [4,9,0,5,1]
+    - Output: 1026
+    - Explanation: 
+        + The root-to-leaf path 4->9->5 represents the number 495.
+        + The root-to-leaf path 4->9->1 represents the number 491.
+        + The root-to-leaf path 4->0 represents the number 40.
+        + Therefore, sum = 495 + 491 + 40 = 1026.
+"""
+class TreeNode: 
+    def __init__(self, val=0, left= None, right= None): 
+        self.val = val 
+        self.left = left 
+        self.right = right
+    
+def sum_numbers(root):
+    if not root: 
+        return []
+    total_traversal = []
+
+    def dfs(node, tmp_lst): 
+        if not node: 
+            return  
+        
+        tmp_lst.append(node.val)
+        if not node.left and not node.right:
+            total_traversal.append(tmp_lst[:])
+        
+        dfs(node.left, tmp_lst)
+        dfs(node.right, tmp_lst)
+        tmp_lst.pop()     # backtrack 
+
+    dfs(root, [])
+    res = []
+    for elem in total_traversal: 
+        if elem not in res: 
+            res.append(elem )
+    res = [["".join(map(str, sub_lst)) for sub_lst in res]]
+    
+    res_int = [int(elem) for elem in res[0]]
+
+    return sum(res_int)
+
+root = TreeNode(4)
+root.left = TreeNode(9)
+root.right = TreeNode(0)
+root.left.left = TreeNode(5)
+root.left.right = TreeNode(1)
+
+print(sum_numbers(root))
