32ac72959b46f997b4199a1d19956c90e4578cdb
can <can.dvg@softworld.jp>
2025-03-25 09:14:52 +0700

    add assignment 130

--- LeetCode/assignment_121_130.py
+++ LeetCode/assignment_121_130.py
@@ -151,9 +151,39 @@ import re
 def is_palindorme(s):
     s = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
     return s == s[::-1]
-    
 
-print(is_palindorme("race a car"))
+"""Assignment 128. Longest Consecutive Sequence.
+
+Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+You must write an algorithm that runs in O(n) time.
+
+Example 1:
+    - Input: nums = [100,4,200,1,3,2]
+    - Output: 4
+    - Explaination: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+
+Example 2:
+    - Input: nums = [0,3,7,2,5,8,4,6,0,1]
+    - Output: 9
+"""
+def longest_consecutive_sequence(nums): 
+    if not nums: 
+        return 0 
+    
+    num_set = set(nums)
+    longest = 0 
+    for num in num_set: 
+        if num -1 not in num_set: 
+            cur_num = num 
+            cur_streak = 1 
+            
+            while cur_num + 1 in num_set: 
+                cur_num += 1 
+                cur_streak += 1 
+            
+            longest = max(longest, cur_streak)
+    return longest 
+    
 
 """Assignment 129. Sum Root to Leaf Numbers
 
@@ -223,3 +253,51 @@ root.left.left = TreeNode(5)
 root.left.right = TreeNode(1)
 
 print(sum_numbers(root))
+
+"""Assignment 129. Surrounded Region.
+
+You are given an m x n matrix board containing letters 'X' and 'O', capture regions that are surrounded:
+
+Connect: A cell is connected to adjacent cells horizontally or vertically.
+Region: To form a region connect every 'O' cell.
+Surround: The region is surrounded with 'X' cells if you can connect the region with 'X' cells and none of the region cells are on the edge of the board.
+To capture a surrounded region, replace all 'O's with 'X's in-place within the original board. You do not need to return anything.
+
+Example 1:
+    - Input: board= [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
+    - Output: [["X","X","X","X"],["X","X","X","X"],["X","X","X","X"],["X","O","X","X"]]
+    
+Example 2:
+    - Input: board = [["X"]]
+    - Output: [["X"]]
+"""
+def solve(board): 
+    n_rows = len(board)
+    n_cols = len(board[0])
+
+    def dfs(row,col): 
+        if row < 0 or col < 0 or row >= n_rows or col >= n_cols or board[row][col] != "O": 
+            return 
+        
+        board[row][col] = "#"
+        dfs(row + 1, col)
+        dfs(row - 1, col)
+        dfs(row, col + 1)
+        dfs(row, col - 1)
+
+    # Step 1: Mark all 'O' connect to the bounder. 
+    for i in range(n_rows):
+        dfs(i, 0)
+        dfs(i, n_cols -1)
+    
+    for j in range(n_cols): 
+        dfs(0, j)
+        dfs(n_rows - 1, j)
+    
+    # Step 2: Update table 
+    for i in range(n_rows): 
+        for j in range(n_cols): 
+            if board[i][j] == "O":
+                board[i][j] = 'X' 
+            elif board[i][j] == '#':
+                board[i][j] = "O"
