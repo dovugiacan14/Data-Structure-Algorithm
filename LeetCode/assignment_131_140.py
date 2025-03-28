dd46ae4cc3a76d5b3e520438db24f68f7e4a84d2
Windows-key <giacanemail@gmail.com>
2025-03-28 09:30:41 +0700

    add assignment 133

--- LeetCode/assignment_131_140.py
+++ LeetCode/assignment_131_140.py
@@ -100,3 +100,73 @@ def min_cut_palindrome(s):
 s = "aab"
 print(min_cut_palindrome(s))
 
+"""Assignment 133: Cloned Graph. 
+
+Given a reference of a node in a connected undirected graph. Return a deep copy (clone) of the graph.
+Each node in the graph contains a value (int) and a list (List[Node]) of its neighbors.
+
+class Node {
+    public int val;
+    public List<Node> neighbors;
+}
+ 
+Test case format:
+
+- For simplicity, each node's value is the same as the node's index (1-indexed). 
+For example, the first node with val == 1, the second node with val == 2, and so on. The graph is represented in the test case using an adjacency list.
+- An adjacency list is a collection of unordered lists used to represent a finite graph. Each list describes the set of neighbors of a node in the graph.
+- The given node will always be the first node with val = 1. You must return the copy of the given node as a reference to the cloned graph.
+
+Example: 
+    - Input: adjList = [[2,4],[1,3],[2,4],[1,3]]
+    - Output: [[2,4],[1,3],[2,4],[1,3]]
+"""
+class Node(object): 
+    def __init__(self, val= 0, neighbors= None):
+        self.val = val 
+        self.neighbors = neighbors if neighbors is not None else []
+
+# DFS Approach 
+def clone_graph(node: Node): 
+    if not node: 
+        return None 
+    
+    old_to_new = {}
+
+    def dfs(node):
+        if node in old_to_new: 
+            return old_to_new[node]
+
+        # clone the node 
+        clone = Node(node.val)
+        old_to_new[node] = clone
+
+        # clone all neighbors 
+        for neighbor in node.neighbors:
+            clone.neighbors.append(dfs(neighbor))
+        
+        return clone 
+    return dfs(node)
+
+# BFS Approach 
+from collections import deque
+def clone_graph(node: Node): 
+    if not node: 
+        return None 
+    
+    old_to_new = {}
+    queue = deque([node])
+    old_to_new[node] = Node(node.val)
+
+    while queue:
+        curr = queue.popleft()
+        for neighor in curr.neighbors: 
+            if neighor not in old_to_new: 
+                old_to_new[neighor] = Node(neighor.val)
+                queue.append(neighor)
+            old_to_new[curr].neighbors.append(old_to_new[neighor])
+    return old_to_new[node]
+        
+
+
+
