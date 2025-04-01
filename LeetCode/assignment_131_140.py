ebe8851a62ee46e336f24cc4cd531421901315d2
can <can.dvg@softworld.jp>
2025-04-01 09:50:35 +0700

    add assignment 138

--- LeetCode/assignment_131_140.py
+++ LeetCode/assignment_131_140.py
@@ -1,4 +1,3 @@
-
 """Assignment 131. Palindrome Partitioning 
 
 Given a string s, partition s such that every substring of the partition is a palindrome. Return all possible palindrome partitioning of s.
@@ -253,5 +252,57 @@ def single_number_v2(nums):
     for key, val in counter.items(): 
         if val == 1: 
             return key
+    
 
+"""Assignment 138. Copy List with Random Pointer. 
+
+A linked list of length n is given such that each node contains an additional random pointer, which could point to any node in the list, or null.
+Construct a deep copy of the list. The deep copy should consist of exactly n brand new nodes, where each new node has its value set to the value of its corresponding original node. 
+Both the next and random pointer of the new nodes should point to new nodes in the copied list such that the pointers in the original list and copied list represent the same list state. 
+None of the pointers in the new list should point to nodes in the original list.
+
+For example, if there are two nodes X and Y in the original list, where X.random --> Y, then for the corresponding two nodes x and y in the copied list, x.random --> y.
+Return the head of the copied linked list.
+
+The linked list is represented in the input/output as a list of n nodes. Each node is represented as a pair of [val, random_index] where:
+    - val: an integer representing Node.val
+    - random_index: the index of the node (range from 0 to n-1) that the random pointer points to, or null if it does not point to any node.
+Your code will only be given the head of the original linked list.
+
+Example 1:
+    - Input: head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+    - Output:  [[7,null],[13,0],[11,4],[10,2],[1,0]]
+
+Example 2:
+    - Input: head = [[1,1],[2,1]]
+    - Output: [[3,null],[3,0],[3,null]]
+"""
+class Node(object): 
+    def __init__(self, x, next= None, random= None): 
+        self.val = int(x)
+        self.next = next 
+        self.random = random  
+
+def copy_random_lst(self, head): 
+    if not head: 
+        return None 
+    
+    # step1: create a mapping from original nodes to new nodes
+    old_to_new = {}
+    current = head 
+    while current: 
+        old_to_new[current] = Node(current.val)
+        current = current.next
+    
+    # step 2: update the next and random pointers for the new nodes 
+    current = head 
+    while current: 
+        if current.next: 
+            old_to_new[current].next = old_to_new[current.next]
+        if current.random: 
+            old_to_new[current].random = old_to_new[current.random]
+        current = current.next
+    
+    # step 3: return the head of the copied list 
+    return old_to_new[head]
 
