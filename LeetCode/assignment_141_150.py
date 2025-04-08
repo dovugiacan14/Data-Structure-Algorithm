5bd5cfe7eaea53aee42e65c2f89d2d4ce48d9c48
can <can.dvg@softworld.jp>
2025-04-08 10:07:26 +0700

    add assignment 145

--- LeetCode/assignment_141_150.py
+++ LeetCode/assignment_141_150.py
@@ -170,3 +170,45 @@ def preorder_traversal(root):
     
     dfs(root)
     return result
+
+"""Assignment 145. Binary Tree Postorder Traversal
+
+Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+Example 1:
+    - Input: root = [1,null,2,3]
+    - Output: [3,2,1]
+
+Example 2:
+    - Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+    - Output: [4,6,7,5,2,9,8,3,1]
+"""
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+    
+def post_order_traversal(root):
+    result = []
+    def dfs(node):
+        if not node: 
+            return 
+        
+        dfs(node.left)
+        dfs(node.right)
+        result.append(node.val)
+
+    dfs(root)
+    return result
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+root.right.right = TreeNode(8)
+root.left.right.left = TreeNode(6)
+root.left.right.right = TreeNode(7)
+root.right.right.left = TreeNode(9)
+print(post_order_traversal(root))
