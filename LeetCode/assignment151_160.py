324396ebd60139dd2acafea6190615ce766516da
can <can.dvg@softworld.jp>
2025-04-18 09:36:59 +0700

    add assignment 155

--- LeetCode/assignment151_160.py
+++ LeetCode/assignment151_160.py
@@ -80,3 +80,56 @@ Example 2:
 """
 def findMin(nums): 
     return min(nums)
+
+"""Assignment 155. Min Stack 
+
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+Implement the MinStack class:
+    - MinStack() initializes the stack object.
+    - void push(int val) pushes the element val onto the stack.
+    - void pop() removes the element on the top of the stack.
+    - int top() gets the top element of the stack.
+    - int getMin() retrieves the minimum element in the stack.
+You must implement a solution with O(1) time complexity for each function.
+
+Example 1:
+    - Input: ["MinStack","push","push","push","getMin","pop","top","getMin"]
+             [[],[-2],[0],[-3],[],[],[],[]]
+    - Output: [null,null,null,null,-3,null,0,-2]
+"""
+class MinStack(object): 
+    def __init__(self): 
+        self.arr = []
+        self.minStack = []
+
+    def push(self, val: int): 
+        self.arr.append(val)
+        if not self.minStack or val <= self.minStack[-1]: 
+            self.minStack.append(val)
+    
+    def pop(self): 
+        if self.arr[-1] == self.minStack[-1]: 
+            self.minStack.pop()
+        self.arr.pop()
+    
+    def top(self):
+        return self.arr[-1]
+    
+    def getMin(self): 
+        return self.minStack[-1]
+    
+class MinStack(object): 
+    def __init__(self): 
+        self.stack = []
+    
+    def push(self, val): 
+        return self.stack.append(val)
+
+    def pop(self): 
+        return self.stack.pop()
+
+    def top(self): 
+        return self.stack[-1]
+
+    def getMin(self): 
+        return min(self.stack)
