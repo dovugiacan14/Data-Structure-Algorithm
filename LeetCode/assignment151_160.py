ba0f7ad364e92d24c99d591c9f1ad9b28f1540db
can <can.dvg@softworld.jp>
2025-04-20 10:54:29 +0700

    add assignment 160 161

--- LeetCode/assignment151_160.py
+++ LeetCode/assignment151_160.py
@@ -133,3 +133,28 @@ class MinStack(object):
 
     def getMin(self): 
         return min(self.stack)
+    
+"""Assignment 160. Intersection of Two Linked Lists
+
+iven the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect.
+If the two linked lists have no intersection at all, return null.
+Example 1:
+    - Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+    - Output: Intersected at '8'
+
+Example 2:
+    - Input: intersectVal = 2, listA = [1,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+    - Output: Intersected at '2'
+"""
+class ListNode(object): 
+    def __init__(self, x): 
+        self.val = x 
+        self.next = None 
+    
+def get_intersection_node(headA, headB):
+    pointA = headA 
+    pointB = headB 
+    while pointA != pointB: 
+        pointA = pointA.next if pointA else headB 
+        pointB = pointB.next if pointB else headA 
+    return pointA 
