38957065313632f6a2e03021a4fd6bd8db3ce4fd
can <can.dvg@softworld.jp>
2025-04-21 08:46:49 +0700

    add assignment 162

--- LeetCode/assignment161_170.py
+++ LeetCode/assignment161_170.py
@@ -21,3 +21,32 @@ Example 2:
 def find_peak_elem(nums): 
     max_par = max(nums)
     return nums.index(max_par)
+
+"""Assignment 162. Maximum Gap
+
+Given an integer array nums, return the maximum difference between two successive elements in its sorted form. 
+If the array contains less than two elements, return 0.
+You must write an algorithm that runs in linear time and uses linear extra space.
+
+Example 1:
+    - Input: nums = [3,6,9,1]
+    - Output: 2
+    - Explaination: The sorted form of the array is [1,3,6,9], either (3,6) or (6,9) has the maximum difference 3.
+
+Example 2:
+    - Input: nums = [10]
+    - Output: 0
+    - Explaination: The array contains less than 2 elements, therefore return 0.
+"""
+def maximum_gap(nums):
+    if len(nums) <= 1: 
+        return 0
+    sorted_nums = sorted(nums)
+    max_gap = 0
+    for i in range(len(nums) - 1): 
+        gap = sorted_nums[i + 1] - sorted_nums[i]
+        max_gap = gap if gap > max_gap else max_gap
+    return max_gap
+
+nums = [3,6,9,1]
+print(maximum_gap(nums))
