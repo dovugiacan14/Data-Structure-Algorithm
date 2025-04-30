f7102a28a1680e2b742024a9a97bb0656a3cfd4f
Windows-key <giacanemail@gmail.com>
2025-04-30 16:50:30 +0700

    add assignment 174

--- LeetCode/assignment_171_180.py
+++ LeetCode/assignment_171_180.py
@@ -120,3 +120,33 @@ class BSTIterator(object):
 
     def hasNext(self):
         return len(self.stack) > 0 
+
+"""Assignment 174. Dungeon Game
+The demons had captured the princess and imprisoned her in the bottom-right corner of a dungeon. The dungeon consists of m x n rooms laid out in a 2D grid. 
+Our valiant knight was initially positioned in the top-left room and must fight his way through dungeon to rescue the princess.
+The knight has an initial health point represented by a positive integer. If at any point his health point drops to 0 or below, he dies immediately.
+Some of the rooms are guarded by demons (represented by negative integers), so the knight loses health upon entering these rooms; 
+other rooms are either empty (represented as 0) or contain magic orbs that increase the knight's health (represented by positive integers
+To reach the princess as quickly as possible, the knight decides to move only rightward or downward in each step.
+Return the knight's minimum initial health so that he can rescue the princess.
+
+Example 1: 
+    - Input: dungeon = [[-2,-3,3],[-5,-10,1],[10,30,-5]]
+    - Output: 7
+    - Explaination: The initial health of the knight must be at least 7 if he follows the optimal path: RIGHT-> RIGHT -> DOWN -> DOWN.
+"""
+
+# Using Dynamic Programming
+def calculate_minimum(dungeon): 
+    m, n = len(dungeon), len(dungeon[0])
+    dp = [[float('inf')] * (n + 1) for _ in range(m+1)]
+    dp[m][n-1] = dp[m-1][n] = 1 # base case: princess cell 
+
+    for i in range(m-1, -1, -1): 
+        for j in range(n-1, -1, -1):
+            need =  min(dp[i+1][j], dp[i][j + 1]) - dungeon[i][j]
+            dp[i][j] = max(1, need)
+    return dp[0][0]
+        
+dungeon = [[-2,-3,3],[-5,-10,1],[10,30,-5]] 
+print(calculate_minimum(dungeon))
