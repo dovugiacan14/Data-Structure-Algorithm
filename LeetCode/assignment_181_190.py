b21a3a170d74a7604743423dec8600edd16d999d
can <can.dvg@softworld.jp>
2025-05-09 08:55:19 +0700

    add assignment 183

--- LeetCode/assignment_181_190.py
+++ LeetCode/assignment_181_190.py
@@ -31,3 +31,14 @@ def duplicate_emails(person: pd.DataFrame):
     df_result = pd.DataFrame({'Email': duplicate_emails})
     return df_result
 
+"""Assignment 183: Customers Who Never Order"""
+def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    need_order_name = []
+    ordered_id = orders["customerId"].to_list()
+    customers_dict = customers.set_index('id')["name"].to_dict()
+    for id, name in customers_dict.items(): 
+        if id not in ordered_id:
+            need_order_name.append(name)
+    df_result = pd.DataFrame({'Customers': need_order_name})
+    return df_result
+
