cbd72e7dfe5d296ec8886438ca54e114386eab06
can <can.dvg@softworld.jp>
2025-05-20 08:12:08 +0700

    add assignment 196

--- LeetCode/assignment_191_200.py
+++ LeetCode/assignment_191_200.py
@@ -1,4 +1,6 @@
+import pandas as pd 
 from collections import Counter
+
 """Assignment 191: Number of 1 Bits
 Given a positive integer n, write a function that returns the number of set bits in its binary representation (also known as the Hamming weight).
 
@@ -18,7 +20,23 @@ Example 3:
     - Explaination: The input binary string 1111111111111111111111111111101 has a total of thirty set bits.
 """
 
+
 def hamming_weight(n: int) -> int:
     binary_bits = bin(n)[2:]
     freq = Counter(binary_bits)
     return freq.get("1", 0)
+
+
+"""Assignment 196: Delete Duplicate Emails
+Write a solution to delete all duplicate emails, keeping only one unique email with the smallest id.
+For SQL users, please note that you are supposed to write a DELETE statement and not a SELECT one.
+For Pandas users, please note that you are supposed to modify Person in place.
+After running your script, the answer shown is the Person table. The driver will first compile and run your piece of code and then show the Person table. 
+The final order of the Person table does not matter.
+
+"""
+def delete_duplicate_emails(person: pd.DataFrame) -> None:
+    person.sort_values(by= "id", inplace= True)
+    person.drop_duplicates(subset= "email", keep= "first", inplace= True)
+    pass
+
