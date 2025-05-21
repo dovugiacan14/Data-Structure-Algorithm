83d75d35b92234ef65962ff81a9e496c43dc6336
can <can.dvg@softworld.jp>
2025-05-21 08:59:49 +0700

    add assginment 197

--- LeetCode/assignment_191_200.py
+++ LeetCode/assignment_191_200.py
@@ -38,5 +38,36 @@ The final order of the Person table does not matter.
 def delete_duplicate_emails(person: pd.DataFrame) -> None:
     person.sort_values(by= "id", inplace= True)
     person.drop_duplicates(subset= "email", keep= "first", inplace= True)
-    pass
 
+
+"""Assignment 197: Rising Temperature
+Write a solution to find all dates' id with higher temperatures compared to its previous dates (yesterday).
+Return the result table in any order.
+"""
+def rising_temperature(weather: pd.DataFrame):
+    if weather.empty:
+        return pd.DataFrame(columns=["id"])
+    
+    prev_day = weather.copy()
+    prev_day["recordDate"] = prev_day["recordDate"] + pd.Timedelta(days=1)
+    prev_day = prev_day.rename(columns= {
+        "temperature": "prev_temp",
+        "id": "prev_id"
+    })
+
+    merged = weather.merge(prev_day, on= "recordDate")
+    result = merged[merged["temperature"] > merged["prev_temp"]][["id"]]
+    return result.reset_index(drop= True)
+
+data = {
+    "id": [1, 2, 3, 4, 5, 6, 7],
+    "recordDate": [
+        "2023-01-01", "2023-01-02", "2023-01-03", "2023-01-04",
+        "2023-01-05", "2023-01-06", "2023-01-07"
+    ],
+    "temperature": [10, 15, 14, 20, 20, 19, 25]
+}
+
+df = pd.DataFrame(data)
+df["recordDate"] = pd.to_datetime(df["recordDate"])  
+print(rising_temperature(df))
