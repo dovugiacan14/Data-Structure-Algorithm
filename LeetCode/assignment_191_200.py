ec1283e222ce38afefc7873b6a2f062eaef11bbd
can <can.dvg@softworld.jp>
2025-05-23 16:11:45 +0700

    add asignment 198 and 199

--- LeetCode/assignment_191_200.py
+++ LeetCode/assignment_191_200.py
@@ -1,5 +1,5 @@
-import pandas as pd 
-from collections import Counter
+import pandas as pd
+from collections import deque, Counter
 
 """Assignment 191: Number of 1 Bits
 Given a positive integer n, write a function that returns the number of set bits in its binary representation (also known as the Hamming weight).
@@ -35,39 +35,120 @@ After running your script, the answer shown is the Person table. The driver will
 The final order of the Person table does not matter.
 
 """
+
+
 def delete_duplicate_emails(person: pd.DataFrame) -> None:
-    person.sort_values(by= "id", inplace= True)
-    person.drop_duplicates(subset= "email", keep= "first", inplace= True)
+    person.sort_values(by="id", inplace=True)
+    person.drop_duplicates(subset="email", keep="first", inplace=True)
 
 
 """Assignment 197: Rising Temperature
 Write a solution to find all dates' id with higher temperatures compared to its previous dates (yesterday).
 Return the result table in any order.
 """
+
+
 def rising_temperature(weather: pd.DataFrame):
     if weather.empty:
         return pd.DataFrame(columns=["id"])
-    
+
     prev_day = weather.copy()
     prev_day["recordDate"] = prev_day["recordDate"] + pd.Timedelta(days=1)
-    prev_day = prev_day.rename(columns= {
-        "temperature": "prev_temp",
-        "id": "prev_id"
-    })
+    prev_day = prev_day.rename(columns={"temperature": "prev_temp", "id": "prev_id"})
 
-    merged = weather.merge(prev_day, on= "recordDate")
+    merged = weather.merge(prev_day, on="recordDate")
     result = merged[merged["temperature"] > merged["prev_temp"]][["id"]]
-    return result.reset_index(drop= True)
+    return result.reset_index(drop=True)
+
 
 data = {
     "id": [1, 2, 3, 4, 5, 6, 7],
     "recordDate": [
-        "2023-01-01", "2023-01-02", "2023-01-03", "2023-01-04",
-        "2023-01-05", "2023-01-06", "2023-01-07"
+        "2023-01-01",
+        "2023-01-02",
+        "2023-01-03",
+        "2023-01-04",
+        "2023-01-05",
+        "2023-01-06",
+        "2023-01-07",
     ],
-    "temperature": [10, 15, 14, 20, 20, 19, 25]
+    "temperature": [10, 15, 14, 20, 20, 19, 25],
 }
 
 df = pd.DataFrame(data)
-df["recordDate"] = pd.to_datetime(df["recordDate"])  
+df["recordDate"] = pd.to_datetime(df["recordDate"])
 print(rising_temperature(df))
+
+"""Assignment 198: House Robber
+You are a professional robber planning to rob houses along a street. 
+Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected 
+and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+Example 1: 
+    - Input: nums = [1,2,3,1]
+    - Output: 4 
+    - Explaination:  Rob house 1 (money = 1) and then rob house 3 (money = 3).
+    Total amount you can rob = 1 + 3 = 4.
+
+Example 2: 
+    - Input: nums = [2,7,9,3,1]
+    - Output: 12 
+    - Explaination: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+    Total amount you can rob = 2 + 9 + 1 = 12.
+"""
+# Use Dynamic Programming
+def house_robber(nums):
+    n = len(nums)
+    if n == 1: 
+        return nums[0]
+
+    dp = [0] * n 
+    dp[0] = nums[0]
+    dp[1] = max(nums[0], nums[1])
+    for i in range(2, n): 
+        dp[i] = max(dp[i-1], nums[i] + dp[i-2])
+    return dp[-1]
+
+"""Assignment 199: Binary Tree Right Side View
+Given the root of a binary tree, imagine yourself standing on the right side of it.
+Return the values of the nodes you can see ordered from top to bottom.
+
+Example 1: 
+- Input: root = [1,2,3,null,5,null,4] 
+- Output: [1,3,4]
+
+Example 2: 
+- Input: root = [1,2,3,4,null,null,null,5]
+- Output: [1,3,4,5]
+"""
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+    
+    
+    def rightSideView(self, root):
+        """Idea: Use BFS to apply level-order traversal. 
+        Each level we get last node in list node of this level 
+        """
+        if not root: 
+            return []
+        
+        result = []
+        queue = deque([root])
+
+        while queue: 
+            level_size = len(queue)
+            for i in range(level_size): 
+                node = queue.popleft()
+
+                if i == level_size - 1: 
+                    result.append(node.val)
+                if node.left: 
+                    queue.append(node.left)
+                if node.right: 
+                    queue.append(node.right)
+        return result
