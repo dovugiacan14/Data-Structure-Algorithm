c6b02849192710c122b55880d114629d36ad8bbd
Windows-key <giacanemail@gmail.com>
2025-05-24 10:01:41 +0700

    add assugnment 200

--- LeetCode/assignment_191_200.py
+++ LeetCode/assignment_191_200.py
@@ -98,18 +98,21 @@ Example 2:
     - Explaination: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
     Total amount you can rob = 2 + 9 + 1 = 12.
 """
+
+
 # Use Dynamic Programming
 def house_robber(nums):
     n = len(nums)
-    if n == 1: 
+    if n == 1:
         return nums[0]
 
-    dp = [0] * n 
+    dp = [0] * n
     dp[0] = nums[0]
     dp[1] = max(nums[0], nums[1])
-    for i in range(2, n): 
-        dp[i] = max(dp[i-1], nums[i] + dp[i-2])
+    for i in range(2, n):
+        dp[i] = max(dp[i - 1], nums[i] + dp[i - 2])
     return dp[-1]
+
 
 """Assignment 199: Binary Tree Right Side View
 Given the root of a binary tree, imagine yourself standing on the right side of it.
@@ -123,32 +126,84 @@ Example 2:
 - Input: root = [1,2,3,4,null,null,null,5]
 - Output: [1,3,4,5]
 """
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
-    
-    
+
     def rightSideView(self, root):
-        """Idea: Use BFS to apply level-order traversal. 
-        Each level we get last node in list node of this level 
+        """Idea: Use BFS to apply level-order traversal.
+        Each level we get last node in list node of this level
         """
-        if not root: 
+        if not root:
             return []
-        
+
         result = []
         queue = deque([root])
 
-        while queue: 
+        while queue:
             level_size = len(queue)
-            for i in range(level_size): 
+            for i in range(level_size):
                 node = queue.popleft()
 
-                if i == level_size - 1: 
+                if i == level_size - 1:
                     result.append(node.val)
-                if node.left: 
+                if node.left:
                     queue.append(node.left)
-                if node.right: 
+                if node.right:
                     queue.append(node.right)
         return result
+
+
+"""Assignment 200: Number of Islands.
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
+You may assume all four edges of the grid are all surrounded by water.
+
+Example 1: 
+- Input: grid = [
+            ["1","1","1","1","0"],
+            ["1","1","0","1","0"],
+            ["1","1","0","0","0"],
+            ["0","0","0","0","0"]
+        ]
+- Output: 1
+
+Example 2: 
+- Input: grid = [
+            ["1","1","0","0","0"],
+            ["1","1","0","0","0"],
+            ["0","0","1","0","0"],
+            ["0","0","0","1","1"]
+        ]
+- Output: 3
+"""
+def number_of_islands(grid):
+    # Use Depth-First Search (DFS) to solve this problem. 
+    if not grid: 
+        return 0 
+
+    rows, cols = len(grid), len(grid[0])
+    count = 0 
+
+    def dfs(r,c):
+        # stopping condition
+        if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] != "1":
+            return 
+        grid[r][c] = "0"  # mark as visited 
+
+        # recursion to 4 directions 
+        dfs(r + 1, c)
+        dfs(r - 1, c)
+        dfs(r, c + 1)
+        dfs(r, c - 1)
+
+    for i in range(rows): 
+        for j in range(cols): 
+            if grid[i][j] == "1":
+                count += 1 
+                dfs(i, j)
+    return count 
