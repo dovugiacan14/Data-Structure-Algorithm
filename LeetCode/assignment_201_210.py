f7ff156d83d0d4f8ac52760304090b0e197b4a63
can <can.dvg@softworld.jp>
2025-05-29 09:42:16 +0700

    add assignment 205

--- LeetCode/assignment_201_210.py
+++ LeetCode/assignment_201_210.py
@@ -148,3 +148,48 @@ def count_primes(n):
 
 n= 10
 print(count_primes(n))
+
+"""Assignment 205: Isomorphic Strings
+Given two strings s and t, determine if they are isomorphic.
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+All occurrences of a character must be replaced with another character while preserving the order of characters. 
+No two characters may map to the same character, but a character may map to itself.
+
+Example 1: 
+- Input: s = "egg", t = "add"
+- Output: True
+- Explaination: The strings s and t can be made identical by:
+    + Mapping 'e' to 'a'.
+    + Mapping 'g' to 'd'.
+
+Example 2: 
+- Input: s = "foo", t = "bar"
+- Output: Fasle
+
+Example 3: 
+- Input: s = "paper", t = "title"
+- Output: True
+
+"""
+
+def isormophic_string(s, t): 
+    if len(s) != len(t): 
+        return False 
+    data_dict = {}
+    list_val = []
+    for i in range(len(s)):
+        s_char=  s[i]
+        t_char = t[i]
+        if s_char in data_dict: 
+            if data_dict[s_char] != t_char:
+                return False 
+        else: 
+            if t[i] in list_val:
+                return False
+            data_dict[s_char] = t[i]
+            list_val.append(t_char)
+    return True 
+
+s = "paper"
+t = "title"
+print(isormophic_string(s, t))
