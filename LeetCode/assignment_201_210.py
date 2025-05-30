590e6ecbbc9c4bafa9713d4901805dc2a5873258
can <can.dvg@softworld.jp>
2025-05-30 09:08:41 +0700

    add assignment 206

--- LeetCode/assignment_201_210.py
+++ LeetCode/assignment_201_210.py
@@ -193,3 +193,38 @@ def isormophic_string(s, t):
 s = "paper"
 t = "title"
 print(isormophic_string(s, t))
+
+"""Assignment 206: Reverse Linked List
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Example 1: 
+- Input: head = [1,2,3,4,5]
+- Output: [5,4,3,2,1]
+
+Example 2: 
+- Input: head = [1,2]
+- Output:[2,1]
+
+"""
+class ListNode: 
+    def __init__(self, val, next= None):
+        self.val = val 
+        self.next = next 
+    
+def reverse_linked_list(head):
+    if not head: 
+        return None 
+    
+    arr = []
+    while head:
+        arr.append(head.val)
+        head = head.next
+    
+    new_arr = arr[::-1]
+
+    new_head = ListNode(new_arr[0])
+    cur = new_head
+    for val in new_arr[1:]:
+        cur.next = ListNode(val)
+        cur = cur.next 
+    return new_head
