def66ade57fce85eaa058864891e8dba2b238c5a
Windows-key <giacanemail@gmail.com>
2025-05-31 10:08:11 +0700

    add assignment 207

--- LeetCode/assignment_201_210.py
+++ LeetCode/assignment_201_210.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from collections import defaultdict
 
 """Assignment 201: Binary Tree Right Side View
 Given two integers left and right that represent the range [left, right], return the bitwise AND of all numbers in this range, inclusive.
@@ -228,3 +229,52 @@ def reverse_linked_list(head):
         cur.next = ListNode(val)
         cur = cur.next 
     return new_head
+
+"""Assignment 207: Course Schedule
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. 
+You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+
+For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+Return true if you can finish all courses. Otherwise, return false.
+
+Example 1:
+    - Input: numCourses = 2, prerequisites = [[1,0]]
+    - Output: true
+    - Explanation: There are a total of 2 courses to take. 
+    To take course 1 you should have finished course 0. So it is possible.
+
+Example 2:
+    - Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+    - Output: false
+    - Explanation: There are a total of 2 courses to take. 
+    To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
+"""
+def can_finish(numCourses, prerequisites):
+    """Quy về bài toán đồ thị.
+    - Nếu đồ thị có chu trình thì bạn không thể hoàn thành khóa học -> False 
+    - Nếu đồ thị không có chun trình thì bạn có thể hoàn thành khóa học -> True 
+    """
+    # Build graph: edges from b -> a (b must be before a)
+    graph = defaultdict(list) 
+    for a, b in prerequisites:
+        graph[b].append(a)
+
+    visited = [0] * numCourses
+
+    def dfs(course):
+        if visited[course] == 1: 
+            return False 
+        if visited[course] == 2: # already visited 
+            return True 
+
+        visited[course] = 1 # mark as visiting 
+        for neighbor in graph[course]:
+            if not dfs(neighbor):
+                return False 
+        visited[course] = 2 # mask as visited 
+        return True 
+    
+    for course in range(numCourses):
+        if not dfs(course):
+            return False 
+    return True 
