9459e70115ab736f84dbe638eaad303d4e30b494
can <can.dvg@softworld.jp>
2025-06-03 15:04:14 +0700

    add assignment 209

--- LeetCode/assignment_201_210.py
+++ LeetCode/assignment_201_210.py
@@ -53,14 +53,16 @@ Example 2:
 - Output: False
 
 """
+
+
 def is_happy(n):
-    def calculate_sum_square(number): 
-       return sum(int(c) ** 2 for c in str(number))
-    
+    def calculate_sum_square(number):
+        return sum(int(c) ** 2 for c in str(number))
+
     seen = set()
-    while n != 1: 
+    while n != 1:
         if n in seen:
-            return False 
+            return False
         seen.add(n)
         n = calculate_sum_square(n)
     return True
@@ -78,24 +80,27 @@ Example 2:
 - Output: 7
 
 """
-class ListNode: 
-    def __init__(self, val, next= None):
-        self.val = val 
-        self.next = next 
-    
+
+
+class ListNode:
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+
+
 def remove_elements(head: Optional[ListNode], val: int):
     if not head:
-        return None 
-    
+        return None
+
     arr = []
     while head:
         arr.append(head.val)
         head = head.next
-    # delete 
+    # delete
     new_list = [x for x in arr if x != val]
     if not new_list:
-        return None 
-    
+        return None
+
     new_head = ListNode(new_list[0])
     cur = new_head
     for val in new_list[1:]:
@@ -121,33 +126,37 @@ Example 3:
 - Output: 0 
 
 """
+
+
 # O(N^2)
 def count_primes(n):
-    if n < 2: 
-        return 0 
+    if n < 2:
+        return 0
     count = 0
     for i in range(2, n):
-        check = 0  
+        check = 0
         for j in range(2, i):
             if i % j == 0:
-                check += 1 
-        if check == 0: 
+                check += 1
+        if check == 0:
             count += 1
-    return count 
+    return count
+
 
 # O(N.log(N))
 def count_primes(n):
-    if n < 2: 
-        return 0 
+    if n < 2:
+        return 0
     is_prime = [True] * n
-    is_prime[0] = is_prime[1] = False 
+    is_prime[0] = is_prime[1] = False
     for i in range(2, int(n**0.5) + 1):
         if is_prime[i]:
-            for j in range(i*i, n, i):
+            for j in range(i * i, n, i):
                 is_prime[j] = False
     return sum(is_prime)
 
-n= 10
+
+n = 10
 print(count_primes(n))
 
 """Assignment 205: Isomorphic Strings
@@ -173,23 +182,25 @@ Example 3:
 
 """
 
-def isormophic_string(s, t): 
-    if len(s) != len(t): 
-        return False 
+
+def isormophic_string(s, t):
+    if len(s) != len(t):
+        return False
     data_dict = {}
     list_val = []
     for i in range(len(s)):
-        s_char=  s[i]
+        s_char = s[i]
         t_char = t[i]
-        if s_char in data_dict: 
+        if s_char in data_dict:
             if data_dict[s_char] != t_char:
-                return False 
-        else: 
+                return False
+        else:
             if t[i] in list_val:
                 return False
             data_dict[s_char] = t[i]
             list_val.append(t_char)
-    return True 
+    return True
+
 
 s = "paper"
 t = "title"
@@ -207,28 +218,32 @@ Example 2:
 - Output:[2,1]
 
 """
-class ListNode: 
-    def __init__(self, val, next= None):
-        self.val = val 
-        self.next = next 
-    
+
+
+class ListNode:
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+
+
 def reverse_linked_list(head):
-    if not head: 
-        return None 
-    
+    if not head:
+        return None
+
     arr = []
     while head:
         arr.append(head.val)
         head = head.next
-    
+
     new_arr = arr[::-1]
 
     new_head = ListNode(new_arr[0])
     cur = new_head
     for val in new_arr[1:]:
         cur.next = ListNode(val)
-        cur = cur.next 
+        cur = cur.next
     return new_head
+
 
 """Assignment 207: Course Schedule
 There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. 
@@ -249,35 +264,38 @@ Example 2:
     - Explanation: There are a total of 2 courses to take. 
     To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
 """
+
+
 def can_finish(numCourses, prerequisites):
     """Quy về bài toán đồ thị.
-    - Nếu đồ thị có chu trình thì bạn không thể hoàn thành khóa học -> False 
-    - Nếu đồ thị không có chun trình thì bạn có thể hoàn thành khóa học -> True 
+    - Nếu đồ thị có chu trình thì bạn không thể hoàn thành khóa học -> False
+    - Nếu đồ thị không có chun trình thì bạn có thể hoàn thành khóa học -> True
     """
     # Build graph: edges from b -> a (b must be before a)
-    graph = defaultdict(list) 
+    graph = defaultdict(list)
     for a, b in prerequisites:
         graph[b].append(a)
 
     visited = [0] * numCourses
 
     def dfs(course):
-        if visited[course] == 1: 
-            return False 
-        if visited[course] == 2: # already visited 
-            return True 
+        if visited[course] == 1:
+            return False
+        if visited[course] == 2:  # already visited
+            return True
 
-        visited[course] = 1 # mark as visiting 
+        visited[course] = 1  # mark as visiting
         for neighbor in graph[course]:
             if not dfs(neighbor):
-                return False 
-        visited[course] = 2 # mask as visited 
-        return True 
-    
+                return False
+        visited[course] = 2  # mask as visited
+        return True
+
     for course in range(numCourses):
         if not dfs(course):
-            return False 
-    return True 
+            return False
+    return True
+
 
 """Assignment 208: Implement Trie (Prefix Tree)
 A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings. 
@@ -302,42 +320,83 @@ Example 1:
         + trie.insert("app");
         + trie.search("app");  
 """
-class TrieNode: 
-    def __init__(self): 
-        self.children = {}   # dictionary to store child node 
-        self.is_end = False  # True if the node represents the end of a word 
+
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}  # dictionary to store child node
+        self.is_end = False  # True if the node represents the end of a word
+
 
 class Trie:
     def __init__(self):
-        self.root = TrieNode()    # Initialize root node 
+        self.root = TrieNode()  # Initialize root node
 
-    def insert(self, word: str) -> None: 
-        node = self.root 
-        for char in word: 
-            if char not in node.children: 
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node.children:
                 node.children[char] = TrieNode()
             node = node.children[char]
-        node.is_end = True    # mark the end of word 
+        node.is_end = True  # mark the end of word
 
-    def search(self, word: str) -> bool: 
-        node = self.root 
-        for char in word: 
-            if char not in node.children: 
-                return False 
-            node = node.children[char]
-        return node.is_end   # return True only if it's end of a word 
-
-    def startsWith(self, prefix: str) -> bool: 
+    def search(self, word: str) -> bool:
         node = self.root
-        for char in prefix: 
-            if char not in node.children: 
-                return False 
+        for char in word:
+            if char not in node.children:
+                return False
             node = node.children[char]
-        return True 
+        return node.is_end  # return True only if it's end of a word
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
+
 
 trie = Trie()
 trie.insert("apple")
 
-print(trie.search("apple")) 
-print(trie.search("app"))   
-print(trie.startsWith("app")) 
+print(trie.search("apple"))
+print(trie.search("app"))
+print(trie.startsWith("app"))
+
+"""Assignment 209: Minimum Size Subarray Sum 
+Given an array of positive integers nums and a positive integer target, return the minimal length of a subarray whose sum is greater than or equal to target. 
+If there is no such subarray, return 0 instead.
+
+Example 1:
+    - Input: target = 7, nums = [2,3,1,2,4,3]
+    - Output: 2
+    - Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+
+Example 2: 
+    - Input: target = 4, nums = [1,4,4]
+    - Output: 1 
+
+Example 3: 
+    - Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+    - Output: 0
+"""
+
+def min_subarray_len(target, nums): 
+    left = 0 
+    total = 0 
+    min_len = float("inf")
+
+    for right in range(len(nums)):
+        total += nums[right]
+
+        while total >= target: 
+            min_len = min(min_len, right - left + 1) 
+            total -= nums[left]
+            left += 1 
+    return 0 if min_len == float("inf") else min_len
+    pass
+
+target = 11
+nums  = [1,1,1,1,1,1,1,1]
+print(min_subarray_len(target, nums))
