8cbfee2e1059388130389dec6a4cfe6ef9527a9d
Windows-key <giacanemail@gmail.com>
2025-06-10 17:19:19 +0700

    add assignment 216

--- LeetCode/assignment_211_220.py
+++ LeetCode/assignment_211_220.py
@@ -191,3 +191,48 @@ def find_kth_largest(nums, k):
 nums = [3,2,3,1,2,4,5,5,6]
 k = 4
 print(find_kth_largest(nums, k))
+
+"""Assignment 216: Combination Sum III
+Find all valid combinations of k numbers that sum up to n such that the following conditions are true:
+    - Only numbers 1 through 9 are used.
+    - Each number is used at most once
+
+Return a list of all possible valid combinations. The list must not contain the same combination twice, and the combinations may be returned in any order.
+
+Example 1: 
+- Input: k = 3, n = 7
+- Output: [[1,2,4]]
+- Explaination: 1 + 2 + 4 = 7 
+There are no other valid combinations.
+
+Example 2: 
+- Input: k = 3, n = 9
+- Output: [[1,2,6],[1,3,5],[2,3,4]] 
+- Explaination: 
+    1 + 2 + 6 = 9
+    1 + 3 + 5 = 9
+    2 + 3 + 4 = 9
+There are no other valid combinations.
+
+"""
+def combination_sum_III(k, n):
+    def backtrack(start, target, path): 
+        if len(path) == k: 
+            if target == 0: 
+                results.append(path[:])
+            return 
+
+        for i in range(start, 10):
+            if i > target: 
+                break 
+            path.append(i)
+            backtrack(i + 1, target - i, path)
+            path.pop()
+
+    results = []
+    backtrack(1, n, [])
+    return results
+
+k = 3
+n= 9 
+print(combination_sum_III(k, n))
