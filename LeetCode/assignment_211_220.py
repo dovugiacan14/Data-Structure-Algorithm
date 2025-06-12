a0c66fc85e7f17fd97a35c2fbda17a4fa23f5491
Windows-key <giacanemail@gmail.com>
2025-06-12 10:12:04 +0700

    add assignment 219

--- LeetCode/assignment_211_220.py
+++ LeetCode/assignment_211_220.py
@@ -254,3 +254,44 @@ Example 2:
 def contain_duplicates(nums):
     format_nums = list(set(nums))
     return True if len(format_nums) != len(nums) else False
+
+"""Assignment 219: Constains Duplicate II
+Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+Example 1: 
+- Input: nums = [1,2,3,1], k = 3
+- Output: True
+
+Example 2: 
+- Input: nums = [1,2,3,1,2,3], k = 2
+- Output: False
+
+Example 3: 
+- Input: nums = [1, 0, 1, 1], k = 1
+- Output: True
+
+"""
+def contain_duplicate_II(nums, k): 
+    def contain_duplicates(nums):
+        format_nums = list(set(nums))
+        return True if len(format_nums) != len(nums) else False
+
+    for i in range(len(nums) - k): 
+        consider_nums = nums[i : i+k+1]
+        if contain_duplicates(consider_nums): 
+            return True 
+    return False 
+
+def contain_duplicate_II(nums, k): 
+    seen = set()
+    for i in range(len(nums)):
+        if nums[i] in seen: 
+            return True 
+        seen.add(nums[i])
+        if len(seen) > k: 
+            seen.remove(nums[i - k])
+    return False 
+
+nums = [1,2,3,1,2,3]
+k = 2
+print(contain_duplicate_II(nums, k))
