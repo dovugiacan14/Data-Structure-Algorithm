ae6c261944bcb2fa913fd16cc2324a0b614ce3f6
Windows-key <giacanemail@gmail.com>
2025-06-15 11:17:00 +0700

    add assignment 222

--- LeetCode/assignment_221_230.py
+++ LeetCode/assignment_221_230.py
@@ -12,7 +12,6 @@ Example 1:
 
 """
 
-
 # Use Dynamic Programming
 def maximal_square(matrix):
     if not matrix:
@@ -30,10 +29,41 @@ def maximal_square(matrix):
     return max_size * max_size
 
 
-matrix = [
-    ["1", "0", "1", "0", "0"],
-    ["1", "0", "1", "1", "1"],
-    ["1", "1", "1", "1", "1"],
-    ["1", "0", "0", "1", "0"],
-]
-print(maximal_square(matrix))
+"""Assignment 222: Count Complete Tree Nodes
+Given the root of a complete binary tree, return the number of the nodes in the tree.
+According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. 
+It can have between 1 and 2h nodes inclusive at the last level h.
+Design an algorithm that runs in less than O(n) time complexity.
+
+Example 1:
+- Input: root = [1,2,3,4,5,6]
+- Output: 6
+
+Example 2: 
+- Input: root = [1]
+- Output: 1
+"""
+class TreeNode(object):
+    def __init__(self, val= 0, left= None, right= None):
+        self.val = val 
+        self.left = left 
+        self.right = right
+    
+def countNodes(root):
+    if not root: 
+        return 0
+
+    def get_height(node, is_left):
+        height = 0 
+        while node: 
+            node = node.left if is_left else node.right
+            height += 1 
+        return height 
+        
+    left_height = get_height(root, True)
+    right_height = get_height(root, False)
+
+    if left_height == right_height:
+        return (1 << left_height) - 1
+    else: 
+        return 1 + countNodes(root.left) + countNodes(root.right)
