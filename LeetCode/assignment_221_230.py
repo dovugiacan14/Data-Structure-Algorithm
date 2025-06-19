da35438f13d8bbffcec8a1c9de060d7a0770329d
Windows-key <giacanemail@gmail.com>
2025-06-19 21:34:07 +0700

    add assignment 226

--- LeetCode/assignment_221_230.py
+++ LeetCode/assignment_221_230.py
@@ -149,3 +149,31 @@ def basic_calculate(s):
             result = result * stack.pop() + stack.pop()
         i += 1
     return result
+
+
+"""Assignment 225: Implement Stack using Queues
+"""
+
+"""Assignment 226: Invert Binary Tree
+Given the root of a binary tree, invert the tree, and return its root.
+
+Example 1: 
+- Input: root = [4,2,7,1,3,6,9]
+- Output: [4,7,2,9,6,3,1]
+
+Example 2: 
+- Input: root = [2,1,3]
+- Output: [2,3,1]
+"""
+class TreeNode(object):
+    def __init__(self, val= 0, left= None, right= None):
+        self.val = val 
+        self.left = left
+        self.right = right
+
+def invert_tree(root):
+    if root is None: 
+        return None
+        
+    root.left, root.right= invert_tree(root.right), invert_tree(root.left)
+    return root
