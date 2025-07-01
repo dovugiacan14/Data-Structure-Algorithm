0c26160dfd820ab9d679633af0618cae563c81a6
Windows-key <giacanemail@gmail.com>
2025-07-01 21:15:16 +0700

    add assigment 238

--- LeetCode/assignment_231_240.py
+++ LeetCode/assignment_231_240.py
@@ -174,3 +174,36 @@ def lowest_common_ancestor_II(root, p, q):
     if left and right:
         return root 
     return left if left else right
+
+
+"""Assignment 238: Product of Array Except Self 
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+Example 1: 
+- Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+- Output: 3
+
+Example 2: 
+- Input:root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+- Output: 5
+"""
+def product_except_self(nums):
+    n = len(nums)
+    answer = [1] * n
+
+    # calculate prefix 
+    for i in range(1, n):
+        answer[i] = answer[i-1] * nums[i-1]
+    
+    # calculate suffix 
+    r = 1 
+    for i in range(n-1, -1, -1):
+        answer[i] *= r
+        r *= nums[i]
+    return answer
+
+nums = [1,2,3,4]
+print(product_except_self(nums))
+
