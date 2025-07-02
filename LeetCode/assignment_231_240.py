758f2d88505814dca5d59e7aa355c3e33ebbd975
Windows-key <giacanemail@gmail.com>
2025-07-02 14:13:23 +0700

    add assignment 140

--- LeetCode/assignment_231_240.py
+++ LeetCode/assignment_231_240.py
@@ -207,3 +207,18 @@ def product_except_self(nums):
 nums = [1,2,3,4]
 print(product_except_self(nums))
 
+"""Assignment 240: Search a 2D Matrix II
+Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+    - Integers in each row are sorted in ascending from left to right.
+    - Integers in each column are sorted in ascending from top to bottom.
+
+Example 1: 
+- Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+- Output: True
+"""
+def search_matrix(matrix, target):
+    for i in range(len(matrix)):
+        if target in matrix[i]:
+            return True
+    return False
+
